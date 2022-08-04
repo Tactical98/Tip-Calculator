4dcb1dd906611ba660266434d1e7a7ca6054ec4b
Tactical98 <preciousnwaigbo98@gmail.com>
2022-08-04 08:59:00 +0100

    tip calculator

--- tip.js
+++ tip.js
@@ -1,7 +1,8 @@
-let btn = document.getElementById(btn);
+let btn = document.getElementById('btn');
 
 btn.addEventListener('click',function() {
     let Bill = document.getElementById("Bill").value;
     let Percentage = document.getElementById("Percentage").value;
     let Amount = document.getElementById("Amount").value = Bill / Percentage;
+    document.getElementById("Total").value =parseFloat(Bill) + parseFloat(Amount);
 })
